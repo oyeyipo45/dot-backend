cb5afd7959aa7d47ab3b8dd99f0c6240a9fc71f6
damilola <oyeyipo45@gmail.com>
2022-04-14 17:10:17 +0100

    'update

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import mongoose from 'mongoose';
-import session from 'express-session';
-import path from 'path';
 import cors from 'cors';
-import MongoStore from 'connect-mongo';
 import errorHandler from 'errorhandler';
 import pageRouter from './routes/pageRoutes';
 
 dotenv.config({ path: 'variable.env' });
 
-// const MongoStore = mongo(session);
 
 const app = express();
 
@@ -19,19 +14,6 @@ app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-if (process.env.NODE_ENV !== 'test') {
-  app.use(
-    session({
-      name: process.env.SESSION_NAME,
-      secret: process.env.SECRET,
-      resave: false,
-      saveUninitialized: false,
-      store: MongoStore.create({
-        mongoUrl: process.env.DEV_MONGODB,
-      }),
-    })
-  );
-}
 
 app.use('/pages', pageRouter);
 
